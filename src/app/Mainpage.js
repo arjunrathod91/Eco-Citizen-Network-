4ac446aab3577dca09c08b8ebf3e143e5e334384
ArjunRathod-Remote_Connect <ar885209@gmail.com>
2024-02-19 19:13:14 +0530

    initial commit

---
+++ src/app/Mainpage.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function mainpage() {
+  return (
+    <div className="">
+      <div className="container h-[80vh] w-[100vw] mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <div className="flex-1 flex flex-col justify-center items-start gap-[35px]">
+          <strong className="text-4xl">
+            Mobilizing Communities, Championing Sustainability: Act Locally,
+            Impact Globally.
+          </strong>
+          <p >We are encouraging youth to fight against Climate Change. Connect with us on our social media handles</p>
+          <button class="border-black border-2 text-black rounded-3xl py-2 px-8 focus:outline-none hover:bg-[#6CE262] ">
+            Explore Challenges
+          </button>
+        </div>
+        <div className="flex-1 flex justify-end items-end">
+          <img src={"./Img/imgtheme.png"} alt="" className="h-[500x] w-[500px]" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default mainpage;
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+"use client"
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+function Navbar() {
+    const [hover,setHover] = useState(false)
+  return (
+      <header className="text-gray-600 body-font">
+  <div className=" container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <Link href={'/'} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 cursor-pointer">
+        <img src={'./Img/earth.png'} alt="" className='w-10 h-10 text-white p-2 rounded-full'/>
+      <span className="ml-3 text-xl">Eco-Citizen Network</span>
+    </Link>
+    <nav className="md:ml-auto h-[50px] w-[500px] md:mr-auto flex flex-wrap items-center text-base justify-center border-2 gap-4 rounded-[30px]">
+      <a className="mr-5 px-3 py-[3px] text-white hover:text-black rounded-[7px]  hover:bg-white cursor-pointer">Home</a>
+      <a className="mr-5 px-3 py-[3px] text-white hover:text-black rounded-[7px] hover:bg-white cursor-pointer" onMouseEnter={()=>setHover(true)}>Explore</a>
+      <a className="mr-5 px-3 py-[3px] text-white hover:text-black rounded-[7px] hover:bg-white cursor-pointer">Connect</a>
+      <a className="mr-5 px-3 py-[3px] text-white hover:text-black rounded-[7px] hover:bg-white cursor-pointer">Donate</a>
+    </nav>
+    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Connect To Wallet
+    </button>
+    <div className={hover ? 'flex-col absolute top-[70px] left-[47%] bg-white list-none p-[1px] rounded-[4px]':'none'} onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)}>
+        <li className='hover:bg-[#7C827C40] hover:text-black py-2 px-2  cursor-pointer'>Challenges</li>
+        <li className='hover:bg-[#7C827C40] hover:text-black py-2 px-2  cursor-pointer'>Events</li>
+    </div>
+  </div>
+</header>
+  )
+}
+
+export default Navbar
